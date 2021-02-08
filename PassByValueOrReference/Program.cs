5c634b7c3f110d39d6a6312b3f7a68ab1d7f9128
Bora Baykar <borabaykar@gmail.com>
2021-02-08 19:40:32 +0300

    Added an example for the ref keyword in functions

--- PassByValueOrReference/Program.cs
+++ PassByValueOrReference/Program.cs
@@ -25,6 +25,27 @@ namespace ValueOrReference
             arr2[0] = 777;
             Console.WriteLine(arr1[0]);
             Console.WriteLine(arr1[1]);
+
+            int a = 117;
+            Console.WriteLine("Value of a in main: " + a);
+            PrintNumber1(a);
+            Console.WriteLine("Value of a in main after pass by value: " + a);
+            PrintNumber2(ref a);
+            Console.WriteLine("Value of a in main after pass by reference: " + a);
+        }
+
+        // Pass parameter by value
+        static void PrintNumber1(int a)
+        {
+            a = 7;
+            Console.WriteLine("Value of a in function pass by value: " + a);
+        }
+
+        // The 'ref' keyword allows us to pass the parameter by reference
+        static void PrintNumber2(ref int a)
+        {
+            a = 7;
+            Console.WriteLine("Value of a in pass by reference: " + a);
         }
     }
 }
