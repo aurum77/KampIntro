dcac1db7572709e306d4aed5afa611063a9def21
Bora Baykar <borabaykar@gmail.com>
2021-02-10 16:58:45 +0300

    Add Product Manager class and fill it with some methods

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public static void Add(Product product)
+        {
+            Console.WriteLine("Product " + product.ProductName + " has been added to the basket");
+        }
+        public static void Update(Product product)
+        {
+            Console.WriteLine("Product " + product.ProductName + " has been updated");
+        }
+    }
+}
