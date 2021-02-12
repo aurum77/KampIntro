f913746341f3936f2b50c93afc8d6bca49c1b231
Bora Baykar <borabaykar@gmail.com>
2021-02-13 00:31:54 +0300

    Add MyList class and the main method

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,35 @@
+﻿namespace GenericsIntro
+{
+    // Imitation of the List class
+    // Generic class
+    class MyList<T>
+    {
+        // This value below has no access specifier but 
+        // C# uses private access specifier for the class members
+        T[] _arr;
+        T[] _tmpArr;
+
+        public MyList()
+        {
+            _arr = new T[0];
+        }
+        
+        public void Add(T item)
+        {
+            _tmpArr = _arr;
+            _arr = new T[_arr.Length + 1];
+            for (int i = 0; i < _tmpArr.Length; i++)
+            {
+                _arr[i] = _tmpArr[i];
+            }
+
+            _arr[_arr.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return _arr.Length; }
+        }
+
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names1= new List<string>();
+            names1.Add("Bora");
+            names1.Add("Tuna");
+            names1.Add("Melih");
+
+            MyList<string> names2 = new MyList<string>();
+            names2.Add("Bora");
+            names2.Add("Tuna");
+            names2.Add("Melih");
+
+            Console.WriteLine(names1.Count);
+            Console.WriteLine(names2.Count);
+        }
+    }
+}
