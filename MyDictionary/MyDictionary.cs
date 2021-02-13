45106458052a2e50f4e61a75e21cd86ebaa4ba13
Bora Baykar <borabaykar@gmail.com>
2021-02-13 21:22:52 +0300

    Add MyDictionary generic class project

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _arrKeys;
+        TValue[] _arrValues;
+        TKey[] _tmpArrKeys;
+        TValue[] _tmpArrValues;
+        public MyDictionary()
+        {
+            // Instantiate the arrays
+            _arrValues = new TValue[0];
+            _arrKeys = new TKey[0];
+        }
+        
+        public void Add(TKey key, TValue value)
+        {
+            _tmpArrKeys = _arrKeys;
+            _tmpArrValues = _arrValues;
+
+            _arrKeys = new TKey[_arrKeys.Length + 1];
+            _arrValues = new TValue[_arrValues.Length + 1];
+
+            for (int i = 0; i < _tmpArrKeys.Length; i++)
+            {
+                _arrKeys[i] = _tmpArrKeys[i];
+            }
+
+            for (int i = 0; i < _tmpArrValues.Length; i++)
+            {
+                _arrValues[i] = _tmpArrValues[i];
+            }
+
+            _arrKeys[_arrKeys.Length - 1] = key;
+            _arrValues[_arrValues.Length - 1] = value;  
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("1", "Cake");
+            dictionary.Add("2", "Pasta");
+            dictionary.Add("3", "Kebab");
+
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("1", "Cake");
+            myDictionary.Add("2", "Pasta");
+            myDictionary.Add("3", "Kebab");
+        }
+    }
+}
