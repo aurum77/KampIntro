555a225d7ebb217287c17de26604d718506c81bc
Bora Baykar <borabaykar@gmail.com>
2021-02-14 15:49:56 +0300

    Add OOP2 with examples on inheritance

---
+++ OOP2/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class Customer
+    {
+        // Base class
+        // If properties in a class don't fit the object,
+        // then we should split up the object
+        public int Id { get; set; }
+        public string TurkishIdNum { get; set; }
+        public string CustomerNum { get; set; }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Individual Customer, Corporate Customer
+            // This adheres to the L in the SOLID principles
+            IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.Id = 1;
+            customer1.CustomerNum = "12345";
+            customer1.Name = "Bora";
+            customer1.Surname = "Baykar";
+            customer1.TurkishIdNum = "1338";
+
+            CorporateCustomer customer2 = new CorporateCustomer();
+            customer2.Id = 1;
+            customer2.CustomerNum = "123";
+            customer2.CompanyName = "Kodlama.io";
+            customer2.TaxId = "123123";
+
+            Customer customer3 = new IndividualCustomer();
+            Customer customer4 = new CorporateCustomer();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer3);
+            customerManager.Add(customer4);
+        }
+    }
+}
